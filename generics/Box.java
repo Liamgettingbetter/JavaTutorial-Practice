e3b6475d7fa3857f2750ba5caa74c4e22894a14d
Liam Lei <775138281@qq.com>
2017-02-27 21:40:26 +0800

    updates

---
+++ generics/Box.java
@@ -0,0 +1,25 @@
+package generics;
+
+public class Box<T> {
+	private T t;
+
+	public void set(T t) {
+		this.t = t;
+	}
+
+	public T get() {
+		return t;
+	}
+
+	public <U extends Number> void inspect(U u) {
+		System.out.println("T : " + t.getClass().getName());
+		System.out.println("U : " + u.getClass().getName());
+	}
+
+	public static void main(String[] args) {
+		Box<Integer> integerBox = new Box<>();
+		integerBox.set(new Integer(10));
+		// integerBox.inspect("some text");
+		integerBox.inspect(11);
+	}
+}
