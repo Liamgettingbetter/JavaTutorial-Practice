1ab8803d7e72363564b5ec409ca9876cece41f0e
Liam Lei <775138281@qq.com>
2017-02-27 21:15:24 +0800

    updates

---
+++ Util.java
@@ -0,0 +1,39 @@
+package generics;
+
+public class Util {
+	public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+		return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
+	}
+
+	private static class Pair<K, V> {
+		private K key;
+		private V value;
+
+		public Pair(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public void setKey(K key) {
+			this.key = key;
+		}
+
+		public void setValue(V value) {
+			this.value = value;
+		}
+
+		public K getKey() {
+			return this.key;
+		}
+
+		public V getValue() {
+			return this.value;
+		}
+	}
+
+	public static void main(String[] args) {
+		Pair<Integer, String> p1 = new Pair<>(1, "apple");
+		Pair<Integer, String> p2 = new Pair<>(2, "pear");
+		System.out.println(compare(p1, p2));
+	}
+}
